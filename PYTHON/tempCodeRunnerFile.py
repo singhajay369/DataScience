59d7b239749fe0b1f57501fd3bfd2f9eefe337a1
singhajay <singhajay2542004@gmail.com>
2026-01-12 18:43:44 +0530

    Deleted untitled and junk files

--- PYTHON/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-balance = 1000
-
-def add_money():
-    balance
-    balance = balance + 500
-    print("Balance after adding money:", balance)
-    print(f"Inside Function: {balance}")
-add_money()
-print(f"Outside Function: {balance}")
